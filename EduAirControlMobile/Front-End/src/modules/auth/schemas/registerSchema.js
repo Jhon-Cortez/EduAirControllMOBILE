b42bec4c7f5b269cdd88f701f622af1ed99e25e2
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-09-23 13:49:52 -0500

    fix: correct register validation i18n keys in schema

--- EduAirControlMobile/Front-End/src/modules/auth/schemas/registerSchema.js
+++ EduAirControlMobile/Front-End/src/modules/auth/schemas/registerSchema.js
@@ -2,15 +2,15 @@ import { z } from 'zod'
 
 export const RegisterSchema = z.object({
   name: z.string().min(2, 'errors.name_short'),
-  email: z.string().min(1, 'errors.required:_email').email('errors.invalid_email'),
+  email: z.string().min(1, 'errors.required_email').email('errors.invalid_email'),
   password: z
     .string()
-    .min(8, 'errors.minLength:_password')
+    .min(8, 'errors.password_min')
     .regex(/[A-Z]/, 'errors.uppercaseRequired:_password'),
 
   confirmPassword: z.string(),
 }).refine((data) => data.password === data.confirmPassword, {
-  message: 'errors.passwords_dont_match',
+  message: 'errors.password_match',
   path: ['confirmPassword'],
 })
 
