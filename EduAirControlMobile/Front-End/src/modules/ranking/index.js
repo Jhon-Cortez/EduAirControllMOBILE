78eca01814e9e8a2976bf00f5b7ee2f96f298495
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-09-24 07:04:49 -0500

    feat:settings cleanup (structure, paths, JS)

---
+++ EduAirControlMobile/Front-End/src/modules/ranking/index.js
@@ -0,0 +1,1 @@
+export { default as RankingScreen } from './pages/ranking/RankingScreen.jsx'
---
+++ EduAirControlMobile/Front-End/src/modules/ranking/pages/ranking/RankingScreen.jsx
@@ -0,0 +1,3 @@
+// En mobile, el ranking es la vista principal del dashboard.
+// Se mantiene como módulo independiente para conservar la arquitectura funcional de la web.
+export { default } from '../../../dashboard/pages/dashboard/DashboardScreen.jsx'
