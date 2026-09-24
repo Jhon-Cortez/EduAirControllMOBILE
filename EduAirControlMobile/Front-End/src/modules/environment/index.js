266e88b656445cf6c3a6df9304c4e00de9d78af3
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-09-24 06:00:25 -0500

    feat:Implementation of index.js with environment routes

---
+++ EduAirControlMobile/Front-End/src/modules/environment/index.js
@@ -0,0 +1,4 @@
+export { default as AllEnvironmentsScreen } from './pages/allEnvironments/AllEnvironmentsScreen.jsx'
+export { default as EnvironmentDetailScreen } from './pages/environmentDetail/EnvironmentDetailScreen.jsx'
+export { default as EnvironmentManagementScreen } from './pages/environmentManagement/EnvironmentManagementScreen.jsx'
+export { default as environmentService } from './services/environmentService.js'
